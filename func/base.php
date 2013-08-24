f494e4a1f90de6987174c0135f501fbf664bcac4
doz10us <doz10us@gmail.com>
2013-08-24 19:15:55 +0400

    Alpha version of logging Merged actual production version into develop

--- func/base.php
+++ func/base.php
@@ -1,26 +1,30 @@
 <?php
+include_once("login.php");
 function ns_connect(&$data) {
-$hostname = getauth('nshost');
-$username = getauth('nsuser');
-$password = getauth('nspass');
+$hostname = $nshost;
+$username = $nsuser;
+$password = $nspass;
 
 $db = mysql_connect($hostname, $username, $password) or die('connect to database failed');
 mysql_set_charset('utf8');
 mysql_select_db('netmap') or die('db not found');
 
+if (!($data['mac'])) {
 // Get MAC from IP
 $query = "select mac from unetmap_host where INET_NTOA(ip) = '".$data['ip']."'";
-$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
+$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
 
 if (mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_assoc($result)) {
 		$data['mac']=$row['mac'];
     }
+} 
 }
-
+if ($data['mac']) {
+	//echo "Mac found <br>";
 // Get Switch ID and port where MAC was seen last time
 $query = "Select switch_id, port from unetmap_host where mac = '".$data['mac']."'";
-$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
+$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
 
 if (mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_assoc($result)) {
@@ -31,22 +35,23 @@ if (mysql_num_rows($result) > 0) {
 
 // And get switch name by its id
 $query = "SELECT name FROM unetmap_host WHERE id = '".$data['swname']."'";
-$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' . mysql_error() . ' query: ' . $sql);
+$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' . mysql_error() . ' query: ' . $query);
 
 if (mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_assoc($result)) {
 		$data['swname']=$row['name'];
     }
 }
-
+//echo "{$data['swname']}";
+}
 mysql_close($db);
 return("OK");
 }
 
-function fhosting($data, &$mydata, $trigger) {
-$hostname = getauth('fhhost');
-$username = getauth('fhuser');
-$password = getauth('fhpass');
+function fhosting(&$data, &$mydata, &$trigger) {
+$hostname = $fhhost;
+$username = $fhuser;
+$password = $fhpass;
 
 $db = mysql_connect($hostname, $username, $password) or die('connect to database failed');
 mysql_select_db('portmap') or die('db not found');
@@ -54,13 +59,28 @@ mysql_select_db('portmap') or die('db not found');
 switch ($trigger) {
 	case "SELECT" :
 		if (!($data['mac'])) { $data['mac'] = $mydata['mac']; } else { $mydata['mac'] = $data['mac']; }
-		$query="SELECT INET_NTOA(ip),port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE mac = '".$data['mac']."'";
-		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
-
+		//echo "{$data['mac']} <br>";
+		//echo "{$mydata['mac']} <br>";
+		//echo "{$data['ip']} <br>";
+		//echo "{$mydata['ip']} <br>";
+		//var_dump($result);
+		if (empty($data['mac']) && empty($mydata['mac'])) {
+			$query="SELECT INET_NTOA(ip),mac,port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE ip = INET_ATON('".$mydata['ip']."')";
+			$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
+		} else {
+			$query="SELECT INET_NTOA(ip),mac,port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE mac = '".$data['mac']."'";
+			$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
+		}
+		
+		//echo "{$data['mac']} <br>";
+		//echo "{$mydata['mac']} <br>";
+		//echo "{$data['ip']} <br>";
+		//echo "{$mydata['ip']} <br>";
+		//var_dump($result);
 		if (mysql_num_rows($result) > 0) {
 			while ($row = mysql_fetch_assoc($result)) {
 				$mydata['ip']	= $row['INET_NTOA(ip)'];
-				$mydata['mac']	= $data['mac'];
+				$mydata['mac']	= $row['mac'];
 				$mydata['name']	= $row['workstation'];
 				$mydata['building'] = $row['building'];
 				$mydata['floor'] = $row['floor'];
@@ -71,6 +91,9 @@ switch ($trigger) {
 				$mydata['update'] = $row['DATE(`update`)']." ".$row['TIME(`update`)'];
 				$mydata['history'] = $row['history'];
 			}
+			//echo "{$data['mac']} <br>";
+			//echo "{$mydata['mac']} <br>";
+			if (!($data['mac'])) { $data['mac'] = $mydata['mac']; ns_connect($data); }
 			$trigger = "UPDATE";
 			echo "I know you!";
 		} else {
@@ -79,19 +102,30 @@ switch ($trigger) {
 		}
 		break;
 	case "INSERT" :
+		//echo "{$mydata['mac']} <br>";
 		$query="INSERT INTO netmap (mac,ip,port,switch_id,workstation,building,floor,room,description) VALUES ('".$mydata['mac']."',INET_ATON('".$mydata['ip']."'),'".$mydata['port']."','".$mydata['swname']."','".$mydata['name']."','".$mydata['building']."','".$mydata['floor']."','".$mydata['room']."','".$mydata['description']."')";
-		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
-		fhosting($data, $mydata, "SELECT", $auto);
+		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
+		$trigger = "SELECT";
+		fhosting($data, $mydata, $trigger);
 		break;
 	case "UPDATE" :
-		$query="UPDATE `netmap` SET `ip`=INET_ATON('".$mydata['ip']."'),`port`='".$mydata['port']."',`switch_id`='".$mydata['swname']."',`workstation`='".$mydata['name']."',`building`='".$mydata['building']."',`floor`='".$mydata['floor']."',`room`='".$mydata['room']."',`description`='".$mydata['description']."',`history`='".$mydata['mac']." ".$mydata['ip']." ".$mydata['port']." ".$mydata['swname']." ".$mydata['name']." ".$mydata['building']." ".$mydata['floor']." ".$mydata['room']." ".$mydata['description']." ".$mydata['update']." \n".$mydata['history']."' WHERE `mac`='".$mydata['mac']."';";
-		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
-		fhosting($data, $mydata, "SELECT", $auto);
+		//echo "{$mydata['mac']} <br>";
+		$query="UPDATE `netmap` SET `ip`=INET_ATON('".$mydata['ip']."'),`mac`='".$mydata['mac']."',`port`='".$mydata['port']."',`switch_id`='".$mydata['swname']."',`workstation`='".$mydata['name']."',`building`='".$mydata['building']."',`floor`='".$mydata['floor']."',`room`='".$mydata['room']."',`description`='".$mydata['description']."',`history`='".$mydata['mac']." ".$mydata['ip']." ".$mydata['port']." ".$mydata['swname']." ".$mydata['name']." ".$mydata['building']." ".$mydata['floor']." ".$mydata['room']." ".$mydata['description']." ".$mydata['update']." \n".$mydata['history']."' WHERE `mac`='".$mydata['mac']."' OR `ip`=INET_ATON('".$mydata['ip']."');";
+		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
+		$trigger = "SELECT";
+		fhosting($data, $mydata, $trigger);
 		break;
+//	case "NOIP" :
+//		//echo "{$mydata['mac']} <br>";
+//		$query="UPDATE `netmap` SET `mac`='".$mydata['mac']."',`port`='".$mydata['port']."',`switch_id`='".$mydata['swname']."',`workstation`='".$mydata['name']."',`building`='".$mydata['building']."',`floor`='".$mydata['floor']."',`room`='".$mydata['room']."',`description`='".$mydata['description']."',`history`='".$mydata['mac']." ".$mydata['ip']." ".$mydata['port']." ".$mydata['swname']." ".$mydata['name']." ".$mydata['building']." ".$mydata['floor']." ".$mydata['room']." ".$mydata['description']." ".$mydata['update']." \n".$mydata['history']."' WHERE `ip`=INET_ATON('".$mydata['ip']."');";
+//		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
+//		$trigger = "SELECT";
+//		fhosting($data, $mydata, $trigger);
+//		break;
 	case "AUTO" :
 		if (!($data['mac'])) { $data['mac'] = $mydata['mac']; } else { $mydata['mac'] = $data['mac']; }
 		$query="SELECT INET_NTOA(ip),port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE mac = '".$data['mac']."'";
-		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
+		$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
 
 		if (mysql_num_rows($result) > 0) {
 			while ($row = mysql_fetch_assoc($result)) {
@@ -108,10 +142,10 @@ switch ($trigger) {
 				$mydata['history'] = $row['history'];
 			}
 			$query="UPDATE `netmap` SET `ip`=INET_ATON('".$mydata['ip']."'),`port`='".$mydata['port']."',`switch_id`='".$mydata['swname']."',`workstation`='".$mydata['name']."',`building`='".$mydata['building']."',`floor`='".$mydata['floor']."',`room`='".$mydata['room']."',`description`='".$mydata['description']."',`history`='".$mydata['mac']." ".$mydata['ip']." ".$mydata['port']." ".$mydata['swname']." ".$mydata['name']." ".$mydata['building']." ".$mydata['floor']." ".$mydata['room']." ".$mydata['description']." ".$mydata['update']." \n".$mydata['history']."' WHERE `mac`='".$mydata['mac']."';";
-			$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
+			$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
 		} else {
 			$query="INSERT INTO netmap (mac,ip,port,switch_id,workstation,building,floor,room,description) VALUES ('".$mydata['mac']."',INET_ATON('".$mydata['ip']."'),'".$mydata['port']."','".$mydata['swname']."','".$mydata['name']."','".$mydata['building']."','".$mydata['floor']."','".$mydata['room']."','".$mydata['description']."')";
-			$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $sql);
+			$result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
 		}
 		break;
 	default:
--- func/get.php
+++ func/get.php
@@ -30,30 +30,38 @@ function checkip($ip) {	// ---- This'll check if IP's from our pool
 }
 
 function getdata(&$data, &$mydata, &$trigger) {
-	if ($_POST['logout']) {
-		header('Location: index.php?logout');
-		exit;
-	} elseif ($_POST['submit']) {
-		$mydata['ip'] = getip(); //$_REQUEST['ip'];
-		$mydata['mac'] = $_REQUEST['mac'];
-		$mydata['swname'] = $_REQUEST['swname'];
-		$mydata['port'] = $_REQUEST['port'];
-		$mydata['name'] = $_REQUEST['name'];
-		$mydata['building'] = $_REQUEST['building'];
-		$mydata['floor'] = $_REQUEST['floor'];
-		$mydata['description'] = $_REQUEST['description'];
-		$mydata['room'] = $_REQUEST['room'];
-		$mydata['history'] = $_REQUEST['history'];
-		$mydata['update'] = $_REQUEST['update'];
-		$data = $mydata;
-		$trigger = htmlspecialchars($_REQUEST['trigger']);
-		foreach($mydata as $key => &$val){
-			$val = htmlspecialchars($val);
-		}
-		unset($val); // Without it, $val will remain alive and containing link to last value of $mydata
-	} else {
-		$data['ip'] = getip();
-		$trigger = "SELECT";
-	}
+    if (isset($_POST)) {
+        if (isset($_POST['logout'])) {
+            header('Location: index.php?logout');
+            exit;
+        } elseif (isset($_POST['submit'])) {
+            $mydata['ip'] = getip(); //$_REQUEST['ip'];
+            //$mydata['ip'] = "212.193.33.253";
+            $mydata['mac'] = $_REQUEST['mac'];
+            $mydata['swname'] = $_REQUEST['swname'];
+            $mydata['port'] = $_REQUEST['port'];
+            $mydata['name'] = $_REQUEST['name'];
+            $mydata['building'] = $_REQUEST['building'];
+            $mydata['floor'] = $_REQUEST['floor'];
+            $mydata['description'] = $_REQUEST['description'];
+            $mydata['room'] = $_REQUEST['room'];
+            $mydata['history'] = $_REQUEST['history'];
+            $mydata['update'] = $_REQUEST['update'];
+            $data = $mydata;
+            $trigger = htmlspecialchars($_REQUEST['trigger'], ENT_QUOTES);
+            foreach($mydata as $key => &$val){
+                $val = htmlspecialchars($val, ENT_QUOTES);
+            }
+            unset($val); // Without it, $val will remain alive and containing link to last value of $mydata
+            foreach($mydata as $key => &$val){
+                $val = trim($val);
+            }
+            unset($val);
+        } else die('Invalid data sent');
+    } else {
+        //$data['ip'] = "212.193.33.253";
+        $data['ip'] = getip();
+        $trigger = "SELECT";
+    }
 }
 ?>
