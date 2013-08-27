aa97517007a48bce09be58c207bed340932664f2
doz10us <doz10us@gmail.com>
2013-08-27 20:42:12 +0400

    Beta version of log output Alpha version of base output

--- func/baseout.php
+++ func/baseout.php
@@ -23,7 +23,7 @@ if (((isset($_SESSION['user_id']) && ((time() - $_SESSION['user_id']) < 300 ))) 
     mysql_set_charset('utf8');
     mysql_select_db('portmap') or die('db not found');
 
-    echo '<form method="post">';
+    //echo '<form method="post">';
     echo '<table border>';
     echo '<tr>';
     echo '<td>IP</td>';
@@ -37,30 +37,27 @@ if (((isset($_SESSION['user_id']) && ((time() - $_SESSION['user_id']) < 300 ))) 
     echo '<td>update</td>';
     echo '<td>descr</td>';
     echo '<tr>';
-    $query="SELECT * FROM netmap";
+    $query="SELECT * FROM `netmap`";
     $result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
     unset($query);
     if (mysql_num_rows($result) > 0) {
         while ($row = mysql_fetch_assoc($result)) {
-            $temp['1']	= $row['INET_NTOA(ip)'];
-            $temp['2']	= $row['mac'];
-            $temp['7'] = $row['switch_id'];
-            $temp['9'] = $row['port'];
-            $temp['3']	= $row['workstation'];
-            $temp['4'] = $row['building'];
-            $temp['5'] = $row['floor'];
-            $temp['6'] = $row['room'];
-            $temp['7'] = $row['description'];
-            $temp['10'] = $row['DATE(`update`)']." ".$row['TIME(`update`)'];
+            echo '<tr>';
+            echo '<td>.$row['INET_NTOA(ip)'].</td>';
+            echo '<td>.$row['mac'].</td>';
+            echo '<td>.$row['switch_id'].</td>';
+            echo '<td>.$row['port'].</td>';
+            echo '<td>.$row['workstation'].</td>';
+            echo '<td>.$row['building'].</td>';
+            echo '<td>.$row['floor'].</td>';
+            echo '<td>.$row['room'].</td>';
+            echo '<td>.$row['description'].</td>';
+            echo '<td>.$row['DATE(`update`)']." ".$row['TIME(`update`)'].</td>';
+            echo '</tr>';
         }
-        echo '<tr>';
-        for ($i = 1; $i<11; $i++){
-            echo '<td>'.$temp[$i].'</td>';
-        }
-        echo '<tr>';
     }
     echo '</table>';
-    echo '</form>';
+    //echo '</form>';
 } else {
     unset($_SESSION['user_id']);
     setcookie('login', '', 0, "/");
