7e2414f8e2c336ad4ce24a16a8d1ad600775855e
doz10us <doz10us@gmail.com>
2013-08-27 18:26:17 +0400

    Log.php debugging

--- func/base.php
+++ func/base.php
@@ -65,6 +65,7 @@ function fhosting(&$data, &$mydata, &$trigger) {
 
     if (!(empty($data['mac']))) { $mydata['mac'] = $data['mac']; } else { $data['mac'] = $mydata['mac']; }
     if (empty($data['mac']) && empty($mydata['mac'])) {
+        echo "empty mac <br>";
         $query="SELECT INET_NTOA(ip),mac,port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE ip = INET_ATON('".$mydata['ip']."')";
         $result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
     } else {
@@ -89,6 +90,7 @@ function fhosting(&$data, &$mydata, &$trigger) {
         if (empty($data['mac'])) {
             $data['mac'] = $temp['mac'];
             ns_connect($data);
+            echo "was nsconnect <br>";
             $mydata['swname'] = $data['swname'];
             $mydata['port'] = $data['port'];
         }
