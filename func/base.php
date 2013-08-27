edaf389eaa2012e9685d959be262ec1da2f201c9
doz10us <doz10us@gmail.com>
2013-08-27 18:31:14 +0400

    Log.php debugging

--- func/base.php
+++ func/base.php
@@ -65,8 +65,8 @@ function fhosting(&$data, &$mydata, &$trigger) {
 
     if (!(empty($data['mac']))) { $mydata['mac'] = $data['mac']; } else { $data['mac'] = $mydata['mac']; }
     if (empty($data['mac']) && empty($mydata['mac'])) {
-        echo "empty mac <br>".$mydata['ip']."<br>";
-        $query="SELECT INET_NTOA(ip),mac,port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE ip = INET_ATON('".$mydata['ip']."')";
+        echo "empty mac <br>".$data['ip']."<br>";
+        $query="SELECT INET_NTOA(ip),mac,port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE ip = INET_ATON('".$data['ip']."')";
         $result = mysql_query($query) or trigger_error(mysql_errno() . ' ' .mysql_error() . ' query: ' . $query);
     } else {
         $query="SELECT INET_NTOA(ip),mac,port,switch_id,workstation,building,floor,room,DATE(`update`),TIME(`update`),description,history FROM netmap WHERE mac = '".$data['mac']."'";
