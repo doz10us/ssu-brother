fc62f9c38c2afe613b6c9b235a4e665d25329429
doz10us <doz10us@gmail.com>
2013-08-27 23:52:34 +0400

    IP check is now shorter Log output is 20 strings

--- func/access.php
+++ func/access.php
@@ -22,13 +22,16 @@ if (((isset($_SESSION['user_id']) && ((time() - $_SESSION['user_id']) < 300 ))) 
     echo '<td>IP</td>';
     echo '<td>Действие</td>';
     echo '<tr>';
+    $i=0;
     foreach ($file as $num => $content) {
         $split = split(" ", $content, 3);
         echo '<tr>';
-        echo '<td>'.date(U, $split[0]).'</td>';
+        echo '<td>'.date("H:i:s", $split[0]).'</td>';
         echo '<td>'.$split[1].'</td>';
         echo '<td>'.$split[2].'</td>';
         echo '<tr>';
+        if (!($i<20)) break;
+        $i++;
     }
     echo '</table>';
     echo '</form>';
